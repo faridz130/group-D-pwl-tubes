39daa374613e583fe11e86fcfc608c908f93b1bb
fardz <faridzazzarqi39@gmail.com>
2024-01-04 21:35:12 +0700

    fix(dashboard) memperbaiki dashboard dan menggunakan layouts template inheritance laravel [faridz]

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -15,7 +15,7 @@
       <ul class="sidebar-menu" data-widget="tree">
 
         <li>
-            <a href="">
+            <a href="{{ route('dashboard')}}">
                 <i class="fa fa-dashboard"></i> <span>Dashboard</span>
             </a>
         </li>
